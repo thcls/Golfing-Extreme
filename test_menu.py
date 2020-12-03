718ee076a50c4d29936334db0743e3ed7fb9c7fa
Thiago Carlos Silva Pereira <49200148+thcls@users.noreply.github.com>
2020-12-03 10:20:52 -0300

    Update test_menu.py

--- test_menu.py
+++ test_menu.py
@@ -3,6 +3,9 @@ from menu import Menu
 from constants import *
 
 pygame.init()
+
+# BLACK BOX
+
 def test_animationclose():
     menu = Menu()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -15,6 +18,9 @@ def test_animation():
     exepect= menu.animation(screen)
     assert exepect == True
 
+
+# WHITE BOX
+    
 def test_initial_menustart():
     menu = Menu()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -26,6 +32,3 @@ def test_initial_menuexit():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     exepect = menu.initial_menu(screen)
     assert exepect == False
-
-# WHITE BOX
-# BLACK BOX
